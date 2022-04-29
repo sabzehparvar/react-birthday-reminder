fc099b593b57f8fa9dd97d895d14c5b91527812b
sabzehparvar <sabzehparvar.mojtaba@gmail.com>
2022-04-29 17:49:58 +0430

    buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import List from './List';
+import List from "./List";
 import data from "./data";
-
 
 const App = () => {
   const [people, setPeople] = useState(data);
-  return ( <>
-    <div className="container p-5 birthday-card ">
-      <div className="card mt-5">
-        <h4 className=" card-body text-center text-capitalize birthday">birthdays today</h4>
-        <div className=" card-body border-top "><List people={people}></List></div>
-        
-        <div className="text-center">
-          <button
-            onClick={() => {
-              console.log("hiii");
-            }}
-            className="btn btn-md btn-warning shadow p-2 w-75 mb-5"
-          >
-            clear all
-          </button>
+
+  return (
+    <>
+      <div className="container p-5 birthday-card ">
+        <div className="card mt-5">
+          <h4 className=" card-body text-center text-capitalize birthday">
+            There are {people.length} birthdays today
+          </h4>
+          <div className=" card-body border-top ">
+            <List people={people} handleDelete={handleDelete}></List>
+          </div>
+
+          <div className="text-center">
+            <button
+              onClick={() => {
+                setPeople([]);
+              }}
+              className="btn btn-md btn-warning shadow p-2 w-75 mb-5"
+            >
+              Clear all
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </> );
-}
- 
+    </>
+  );
+
+  function handleDelete(user) {
+    const id = user.id;
+    const users = [...people];
+
+    const newUsers = users.filter((user) => {
+      return user.id !== id;
+    });
+    setPeople(newUsers);
+  }
+};
+
 export default App;
--- src/List.jsx
+++ src/List.jsx
@@ -1,20 +1,30 @@
-const List = ({ people }) => {
+const List = ({ people, handleDelete }) => {
   return (
     <>
       {people.map((people) => {
         const { id, first_name, last_name, avatar, age } = people;
         return (
           <>
-            <div key={id} className="text-center">
+            <div key={id} className=" row mb-5">
               <img
                 src={avatar}
                 alt={first_name}
-                className=" rounded-circle w-25 shadowb "
+                className=" rounded-circle shadowb col-4 "
               />
-              <h5 className="p-1 m-2 shadow-sm name">
-                {first_name} {last_name}
-              </h5>
-              <h6 className="p-1 m-2 shadow-sm age mb-5">{age}</h6>
+              <div className="col-8 d-flex flex-column mt-1">
+                <h5 className=" shadow-sm name ">
+                  {first_name} {last_name}
+                </h5>
+                <h6 className=" shadow-sm age ">{age}</h6>
+                <button
+                  onClick={() => {
+                    handleDelete(people);
+                  }}
+                  className="btn btn-sm btn-outline-danger"
+                >
+                  delete
+                </button>
+              </div>
             </div>
           </>
         );
